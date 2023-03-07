93b25039aaeb23904cf3de3b49b14d9c6c9bc679
agustin_natalio <agu_natalio@hotmail.com>
2023-03-06 22:14:43 -0300

    Cambio al hero y adicion de script JS

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*let ingreso = parseFloat(prompt("Ingresa un numero mogolico"))
+
+if(ingreso < 10) {
+    console.log("El numerico es menor a 10, el numerico es: "+ingreso)
+}else{
+    console.log("El numerico es mayor a 10, el numerico es: "+ingreso)
+}
+
+let resultSuma = 10;
+let ingresoDelAlumno = parseFloat(prompt(Ingrese el valor de la siguiente operacion: 2 * 5));*/
+
+//variables booleanas
+
+let verdadero = ingresoDelAlumno == resultSuma;
+
+console.log(verdadero);
+
+if(verdadero) {
+    console.log("Muy bien pibe, pasa") 
+}else{
+    console.log("Sos lo mas pelotudo que vi en mi vida, poco queda ya, no sobreviviras al final de todo")
+}
